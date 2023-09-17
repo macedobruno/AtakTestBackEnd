744ad47ac2a420beb718acfab336220f0973e889
brunoms@dcomp.ufs.br <b2*Fox7997>
2023-09-16 23:00:09 -0300

    correcao de encoding

--- BMSAtakTest/app/Http/Controllers/Search.php
+++ BMSAtakTest/app/Http/Controllers/Search.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class Search extends Controller
 {
     function GetGSearch (Request $req){
+        
+        
+        ini_set( 'default_charset', 'utf-8');
+        header('Content-Type: text/html; charset=utf-8');
+
         // converte texto recebido no formato para URL
         $q=urlencode($req->q);
 
@@ -15,6 +20,11 @@ class Search extends Controller
 
         // recebe conteudo da página
         $content = file_get_contents($url);
+
+        //$content = mb_convert_encoding($content, "UTF-8", "HTML-ENTITIES", true);
+
+        $content = mb_convert_encoding($content, 'UTF-8',
+            mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
 
         // busca conteudo dentro da div com o título
         preg_match_all('/<div class="BNeawe vvjwJb AP7Wnd">([^<]*)<\/div>/s', $content, $titles);
@@ -37,7 +47,8 @@ class Search extends Controller
         // loop que povoa array tupla
         //for($i=0; $i < sizeof($titles[1]); $i++){
         for($i=0; $i < sizeof($links); $i++){
-            $tupla[mb_convert_encoding($titles[1][$i], 'UTF-8')] = $links[$i];
+            //$tupla[mb_convert_encoding($titles[1][$i], 'UTF-8')] = $links[$i];
+            $tupla[$titles[1][$i]] = $links[$i];
 
             //echo ("<a href='http://".$links[$i]."/'>".$links[$i]."</a>\n");
         }
