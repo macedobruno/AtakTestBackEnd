7bfc3df1006a4caa991613b9de20583b211e42fb
brunoms@dcomp.ufs.br <b2*Fox7997>
2023-09-17 00:10:43 -0300

    corecao de encoding

--- BMSAtakTest/app/Http/Controllers/Search.php
+++ BMSAtakTest/app/Http/Controllers/Search.php
@@ -15,16 +15,17 @@ class Search extends Controller
         $q=urlencode($req->q);
 
         // cria URL
-        $url = 'https://google.com/search?ie=UTF-8&q='.$q;
+        //$url = 'https://google.com/search?ie=UTF-8&q='.$q;
+        $url = 'https://google.com/search?q='.$q;
 
         // recebe conteudo da página
         $content = file_get_contents($url);
 
-        $content = mb_convert_encoding($content, 'UTF-8',
+        $contentenc = mb_convert_encoding($content, 'UTF-8',
             mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
 
         // busca conteudo dentro da div com o título
-        preg_match_all('/<div class="BNeawe vvjwJb AP7Wnd">([^<]*)<\/div>/s', $content, $titles);
+        preg_match_all('/<div class="BNeawe vvjwJb AP7Wnd">([^<]*)<\/div>/s', $contentenc, $titles);
 
         // busca conteudo do link de cada resultado
         preg_match_all('/<div class="egMi0 kCrYT"><a\s+href="\/url\?q=([^"]+)"/', $content, $linksbruto);
@@ -34,14 +35,14 @@ class Search extends Controller
 
         //explode cada item do array linksbruto para remover espaço e conteudo extra
         foreach($linksbruto[1] as $mt){
-            array_push($links, (explode("&", $mt))[0]);
+            array_push($links, urldecode((explode("&", $mt))[0]));
         }
         // array para receber a relação de titulos e links
         $tupla=[];
 
         // loop que povoa array tupla
         for($i=0; $i < sizeof($titles[1]); $i++){
-            $tupla[$titles[1][$i]] = $links[$i];
+            $tupla[html_entity_decode($titles[1][$i])] = $links[$i];
         }
 
         return json_encode($tupla, JSON_FORCE_OBJECT);
